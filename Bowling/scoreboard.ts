7962cce69fe1748562491ad31b5cd9773e62f78f
JolanBrizard <jolan.brizard@epitech.eu>
2021-06-23 11:49:20 +0200

    changed bowling ugly ternary

--- Bowling/scoreboard.ts
+++ Bowling/scoreboard.ts
@@ -14,22 +14,34 @@ export class Scoreboard {
         return this.throwsArray[index] + this.throwsArray[index + 1]  === 10;
     }
 
+    private static getNextThrowValue(nextThrow : number) {
+        return nextThrow ?? nextThrow;
+    }
+
+    private static getNextFromNextThrowValue(nextFromNextThrow : number) {
+        return nextFromNextThrow ?? nextFromNextThrow;
+    }
+
     computeScore() {
         let score = 0;
         let throwIndex = 0;
         for (let index = 0; index < FRAMES_MAX; index ++) {
+
             const nextThrow = this.throwsArray[throwIndex + 1];
             const nextFromNextThrow = this.throwsArray[throwIndex + 2];
+            const nextScore : number = Scoreboard.getNextThrowValue(nextThrow);
+            const nextFromNextScore : number = Scoreboard.getNextFromNextThrowValue(nextFromNextThrow);
+
             if (this.isStrike(throwIndex)) {
-                this.scoreArray.push(score += 10 + ((nextThrow) ? nextThrow : 0) + ((nextFromNextThrow) ? nextFromNextThrow : 0));
+                this.scoreArray.push(score += 10 + nextScore + nextFromNextScore);
                 throwIndex++;
             }
             else if (this.isSpare(throwIndex)) {
-                this.scoreArray.push(score += 10 + ((nextFromNextThrow) ? nextFromNextThrow : 0));
+                this.scoreArray.push(score += 10 + nextFromNextScore);
                 throwIndex += 2;
             }
             else {
-                this.scoreArray.push(score += this.throwsArray[throwIndex] + ((nextThrow) ? nextThrow : 0));
+                this.scoreArray.push(score += this.throwsArray[throwIndex] + nextScore);
                 throwIndex += 2;
             }
         }
